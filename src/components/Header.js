bec4d9494b10258b94a612b344b707d2db4a898a
chrisphine <onyangoketta@gmail.com>
2022-07-17 13:04:25 +0300

    isdarkmode callback  function set on toggle button

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header ({isDarkMode,onDarkModeClick}){
+    return (
+       // <div className={"App " + (isDarkMode ? "dark" : "light")}>
+          <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+              {isDarkMode ? "Dark" : "Light"} Mode
+            </button>
+          </header>
+        //  </div>
+);} 
